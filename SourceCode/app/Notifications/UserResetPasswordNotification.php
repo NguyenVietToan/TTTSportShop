0fdc30ddc3b1c778cdb727b2bfb408ea3f51d6fe
NguyenVietToan <nguyenviettoan161095@gmail.com>
2018-03-26 17:07:07 +0700

    Add routes xác nhận mail

---
+++ SourceCode/app/Notifications/UserResetPasswordNotification.php
@@ -0,0 +1,65 @@
+<?php
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+
+class UserResetPasswordNotification extends Notification
+{
+    use Queueable;
+
+    public $token;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                        ->subject('Đặt lại mật khẩu')
+                        ->greeting('Hello')
+                        ->line('Bạn nhận được email này bởi vì chúng tôi nhận được yêu cầu đặt lại mật khẩu từ tài khoản của bạn')
+                        ->action('Đặt lại mật khẩu', route('password.reset', $this->token))
+                        ->line('Cảm ơn bạn đã sử dụng trang web của chúng tôi!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
